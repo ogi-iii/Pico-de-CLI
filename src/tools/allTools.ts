66ee38d74d87d1153a616f0d82ce5e87e0778519
ogi-iii <kazuki.aape.09@gmail.com>
2026-08-11 09:23:24 +0900

    feat: Add allTools to send to LLM

---
+++ src/tools/allTools.ts
@@ -0,0 +1,6 @@
+import { editFile } from "./editFile";
+import { execCommand } from "./execCommand";
+import { readFile } from "./readFile";
+import { writeFile } from "./writeFile";
+
+export const allTools = [readFile, writeFile, editFile, execCommand];
